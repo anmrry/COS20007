174564d77c1cb5addc4d49c1042e8e70ce1f8304
anmrry <145962765+anmrry@users.noreply.github.com>
2024-04-14 21:53:02 +1000

    Add files via upload

---
+++ Bag.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SwinAdventure
+{
+    public class Bag : Item
+    {
+        private Inventory _inventory;
+
+        public Bag(string[] ids, string name, string desc) : base(ids, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public GameObject Locate(string id)
+        { 
+            if (AreYou(id))
+            {
+                return this;
+            }
+            else if (_inventory.HasItem(id))
+            {
+                return _inventory.Fetch(id);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return $"In {Name} you can see:\n" + _inventory.ItemList;
+            }
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return _inventory;
+            }
+        }
+    }
+}
